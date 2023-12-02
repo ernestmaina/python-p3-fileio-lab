677a40e992bce5750909449858ee0053df1ce550
ErnestMaina <you@example.com>
2023-12-02 09:30:37 +0300

    2 tests passed

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,36 @@
+from pathlib import Path
+
 def write_file(file_name, file_content):
-    pass
+    # Convert PosixPath object to string
+    file_name = str(file_name)
+    
+    # Add the file extension to the file_name
+    file_name_with_extension = file_name + ".txt"
+
+    # Open the file in write mode and write the content
+    with open(file_name_with_extension, "w") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    # Convert PosixPath object to string
+    file_name = str(file_name)
+    
+    # Add the file extension to the file_name
+    file_name_with_extension = file_name + ".txt"
+
+    # Open the file in append mode and append the content
+    with open(file_name_with_extension, "a") as file:
+        file.write("\n" + append_content)
 
 def read_file(file_name):
-    pass
+    # Convert PosixPath object to string
+    file_name = str(file_name)
+    
+    # Add the file extension to the file_name
+    file_name_with_extension = file_name + ".txt"
+
+    # Open the file in read mode and read the content
+    with open(file_name_with_extension, "r") as file:
+        content = file.read()
+    
+    return content
